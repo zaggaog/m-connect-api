5bed93241f04c22191dd4ac3a23adaa2308e0459
ReganLema <reganlema44@gmail.com>
2026-03-13 20:22:56 +0300

    remove get method

--- routes/api.php
+++ routes/api.php
@@ -110,7 +110,6 @@ Route::middleware('auth:api')->group(function () {
     
     // User profile routes
     Route::prefix('users')->group(function () {
-        Route::get('/{id}', [UserController::class, 'show']);
         Route::put('/{id}', [UserController::class, 'update']);
         Route::post('/{id}/avatar', [UserController::class, 'uploadAvatar']);
     });
