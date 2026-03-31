8b7274baa8c7605e4f103e887d184e6102e70f3b
ReganLema <reganlema44@gmail.com>
2026-03-31 10:45:30 +0300

    email sendgrid test

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/debug-mail', function () {
+    $mailers = config('mail.mailers');
+    
+    return response()->json([
+        'default_mailer' => config('mail.default'),
+        'sendgrid_api_config' => $mailers['sendgrid_api'] ?? 'NOT FOUND',
+        'sendgrid_key' => config('services.sendgrid.key') ? 'SET' : 'NOT SET',
+        'from_address' => config('mail.from.address'),
+        'from_name' => config('mail.from.name'),
+        'mail_driver_exists' => class_exists('\Sichikawa\LaravelSendgridDriver\SendgridTransportServiceProvider'),
+    ]);
 });
